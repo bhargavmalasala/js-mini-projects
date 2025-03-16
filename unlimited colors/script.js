1a1b2a33cc92efd2962b40b4aa9032dc7321722a
bhargavmalasala <tzbhargav@gmail.com>
2025-03-16 21:13:58 +0530

    new project

--- unlimited colors/script.js
+++ unlimited colors/script.js
@@ -1,7 +1,23 @@
 const randomColor = function () {
-    const hex = '0123456ABCDEF';
-    let color = '#';
-    for(let i = 0; i < 6; i++){
-        color += hex[Math.floor(Math.random() * hex.length)];
-    }
-}
+  const hex = "0123456ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += hex[Math.floor(Math.random() * hex.length)];
+  }
+  return color;
+};
+
+const startchangingcolor = function () {
+  intervalid = setInterval(changebgcolor, 100);
+  function changebgcolor() {
+    document.body.style.backgroundColor = randomColor();
+  }
+};
+
+const stopchangingcolor = function () {
+    clearInterval(intervalid);
+  }
+
+
+document.querySelector("#start").addEventListener("click", startchangingcolor);
+document.querySelector("#stop").addEventListener("click", stopchangingcolor);
