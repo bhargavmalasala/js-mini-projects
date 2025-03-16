8e3cf97ef68e590bf4569114bd7511af4458a47c
bhargavmalasala <tzbhargav@gmail.com>
2025-03-16 20:54:08 +0530

    small change

---
+++ unlimited colors/script.js
@@ -0,0 +1,7 @@
+const randomColor = function () {
+    const hex = '0123456ABCDEF';
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random() * hex.length)];
+    }
+}
